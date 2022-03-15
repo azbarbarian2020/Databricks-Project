f962d524c21d6cdeb8fed17ae6bd3452b6ba6255
azbarbarian2020 <azbarbarian2020>
2022-03-15 23:28:30 +0000

    This is the first commit

---
+++ Examples/Visualize (1).sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+select state_id as state, COUNT(zip) as nzips
+from uszips_delta_unmanaged
+where state_id not in ('AS','GU','MP','PR','VI')
+GROUP BY state
+order by nzips
+
+-- COMMAND ----------
+
+select state_id as state, sum(population) as population
+from uszips_delta_unmanaged
+where state_id not in ('AS','GU','MP','PR','VI')
+GROUP BY state
+order by state
+
+-- COMMAND ----------
+
+
